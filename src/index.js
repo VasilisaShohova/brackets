4736669f05cf05d6abdd6c220013ad74004ccae5
Vasilisa Shokhava <vasilisa.shokhava@wolterskluwer.com>
2022-01-07 21:10:56 +0300

    feat: add solutions to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const regEx = /[()[\]{}|]/;
+  if (regEx.test(str) === false) {
+    return false;
+  }
+
+// const t = str.replace(/\(\)|\[\]|\{\}/g, "");
+// return str == t ? !str : check(t);
+
+let stack = [];
+  let current;
+  const matchLookup = {
+        "(": ")", 
+        "[": "]", 
+        "{": "}", 
+        "|": "|", 
+        
+      };
+                    
+  for (let i = 0; i < str.length; i++) {
+    current = str[i]; 
+    
+    if (current === '(' || current === '[' || current === "{" ) {
+      stack.push(current);
+      
+    } else if (current === ')' || current === ']' || current === "}") {
+      const lastBracket = stack.pop();
+      
+      if (matchLookup[lastBracket] !== current) { 
+      
+        return false; 
+      }
+    }
+  }
+  
+  return stack.length === 0;
 }
